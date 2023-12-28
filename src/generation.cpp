eba08b5561cb3084335571dc6513cfa41baa7cff
Ankur Ahir <ahir.ankur0429@gmail.com>
2023-12-28 15:50:38 -0800

    added check to see if integer literal exists in generate exit method

--- src/generation.cpp
+++ src/generation.cpp
@@ -1,11 +1,13 @@
-//
-// Created by Ankur Ahir on 12/28/23.
-//
-
 #include <sstream>
+#include <iostream>
 #include "generation.h"
 
 std::string Generator::generate_exit() const {
+    if (!m_root.expr.integer_literal.value.has_value()) {
+        std::cerr << "generating exit produced an error: integer literal value expected but not found" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
     std::stringstream output_stream;
     output_stream << ".global _main\n.align 2\n_main:\n";
     output_stream << "    mov x0, #" << m_root.expr.integer_literal.value.value() << "\n";
