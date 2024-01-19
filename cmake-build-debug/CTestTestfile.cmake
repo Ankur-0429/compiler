4caf715c2339ddf5a37936185d8bf84a338c404b
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-01-18 22:12:09 -0800

    removed build file

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ankurahir/CLionProjects/compiler
-# Build directory: /Users/ankurahir/CLionProjects/compiler/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external")
-subdirs("tests")
--- cmake-build-debug/external/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ankurahir/CLionProjects/compiler/external/googletest/googletest
-# Build directory: /Users/ankurahir/CLionProjects/compiler/cmake-build-debug/external/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
